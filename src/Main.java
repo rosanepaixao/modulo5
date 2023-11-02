5975cedaca37dbf6ade325314e8db36dbc97f756
Rosane da Paixão <133041345+rosanepaixao@users.noreply.github.com>
2023-11-02 13:43:44 -0300

    Modulo 5

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Character characterObj = Character.valueOf('c');
+        Byte byteObj = Byte.valueOf((byte) 3);
+        Short shortObj =  Short.valueOf((short) 50);
+        Integer integerObj = Integer.valueOf((int) 20);
+        Long longObj = Long.valueOf((long) 5);
+        Float floatObj = Float.valueOf((float) 12.5);
+        Double doubleObj = Double.valueOf((double) 15.5);
+        Boolean booleanObj = Boolean.valueOf((boolean) true);
+
+
+    }
+    }
